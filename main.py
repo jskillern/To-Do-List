599a85e44d75ca1f2e4704968ed491db814af48e
jskillern <skillernjonathon95@gmail.com>
2025-08-23 19:59:32 -0400

    Initiate Project

---
+++ main.py
@@ -0,0 +1,25 @@
+#To-Do List
+from functions import *
+
+to_do_list = []
+
+
+def main():
+    print(f"""
+        --------------------------------
+        Welcome back, let's get to work!
+        --------------------------------
+        Here is your current To-Do List  
+        {to_do_list}
+    """)
+    
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
